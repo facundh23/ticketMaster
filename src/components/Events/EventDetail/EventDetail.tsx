e35bfddec1bc56cc3232f02c709369654c98341e
Facundo Herrera <facundhfed@gmail.com>
2024-02-17 22:02:00 +0100

    Event Detail

--- src/components/Events/EventDetail/EventDetail.tsx
+++ src/components/Events/EventDetail/EventDetail.tsx
@@ -1,11 +1,47 @@
 
+import useLikedEvents from '../../../hooks/useLikedEvents'
+import {CiHeart} from 'react-icons/ci';
+import {FaHeart} from 'react-icons/fa';
 import styles from './EventDetail.module.css'
 
+interface Props {
+    name:string;
+    id:string;
+    images:string;
+    info:string;
+    onEventClick:(id:string) => void;
+}
 
+const EventDetail = ({name,id, images, info, onEventClick}:Props) => {
+    const { isEventLiked, toggleEventLiked } = useLikedEvents(id);
 
-const EventDetail = () => {
+    const handleSeeMoreClick = (e: { stopPropagation: () => void; }) => {
+        e.stopPropagation();
+        onEventClick(id);
+    }
+
+    const handleHearthClick = () => {
+        toggleEventLiked();
+    }
     return (
-        <div>EventDetail</div>
+        <div className={styles.eventItemContainer}>
+            <div className={styles.imageContainer}>
+                {
+                    isEventLiked ?
+                    <FaHeart className={styles.hearthImage} onClick={handleHearthClick} />
+                    :
+                    <CiHeart className={styles.hearthImage} onClick={handleHearthClick} />
+                }
+                <img src={images} alt='Artist Image' width={200} height={200} />
+            </div>
+            <div className={styles.eventInfoContainer}>
+                <h4 className={styles.eventInfoTitle}>{name}</h4>
+                <p className={styles.eventInfoParagraph}>{info}</p>
+                <button className={styles.eventInfoBtn} onClick={handleSeeMoreClick}>
+                    More Info
+                </button>
+            </div>
+        </div>
     )
 }
 
